afc0647f00d34f7e06def09ba66894087403040c
WithoutMe <foma_working@mail.ru>
2025-08-09 06:51:39 +0300

    Создать новый менеджер и исключение

---
+++ src/taskmanager/exeptions/ManagerSaveException.java
@@ -0,0 +1,9 @@
+package taskmanager.exeptions;
+
+import java.io.IOException;
+
+public class ManagerSaveException extends RuntimeException {
+    public ManagerSaveException(final String message) {
+        super(message);
+    }
+}
