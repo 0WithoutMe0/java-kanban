af50b9863f849851f06978bcddc1a0d82cd9cd0a
WithoutMe <foma_working@mail.ru>
2025-08-09 06:57:50 +0300

    Поправить checkstyle в ManagerSaveException

--- src/taskmanager/exeptions/ManagerSaveException.java
+++ src/taskmanager/exeptions/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package taskmanager.exeptions;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final String message) {
         super(message);
